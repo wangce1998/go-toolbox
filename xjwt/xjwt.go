f3a01518e0c2dab10d7437d910f594f87bdcef10
wangce <1147734671@qq.com>
2021-12-09 14:47:11 +0800

    且分支

--- xjwt/xjwt.go
+++ xjwt/xjwt.go
@@ -2,9 +2,19 @@ package xjwt
 
 import (
 	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 type JWTClaims struct {
 	jwt.StandardClaims
 	UserID int64 `json:"user_id"`
+}
+
+func New(userID int64) *jwt.Token {
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, JWTClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
+		},
+		UserID: userID,
+	})
 }
