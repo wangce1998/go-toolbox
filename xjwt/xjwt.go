dd6dee66eeeeeeadc6891d9ef699a3451fd855da
wangce <1147734671@qq.com>
2021-12-09 15:13:53 +0800

    x

--- xjwt/xjwt.go
+++ xjwt/xjwt.go
@@ -2,10 +2,17 @@ package xjwt
 
 import (
 	"github.com/dgrijalva/jwt-go"
+	"github.com/wangce1998/go-toolbox/xerror"
 	"time"
 )
 
 const Secret = "wangce1998"
+
+var (
+	TokenNotfound = xerror.New(11001, "未携带令牌")
+	TokenInvalid  = xerror.New(11002, "%s")
+	TokenExpired  = xerror.New(11003, "令牌已过期")
+)
 
 type JWTClaims struct {
 	jwt.StandardClaims
@@ -21,8 +28,26 @@ func NewToken(userID int64) *jwt.Token {
 	})
 }
 
-func Parse(token string, jc *JWTClaims) (*jwt.Token, error) {
-	return jwt.ParseWithClaims(token, jc, func(token *jwt.Token) (interface{}, error) {
+func Parse(tokenStr string, jc *JWTClaims) (*jwt.Token, xerror.XError) {
+	if len(tokenStr) == 0 {
+		return nil, TokenNotfound
+	}
+	token, err := jwt.ParseWithClaims(tokenStr, jc, func(token *jwt.Token) (interface{}, error) {
 		return []byte(Secret), nil
 	})
+
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
+				return token, TokenExpired
+			} else {
+				return token, xerror.New(TokenInvalid.Code(), err.Error())
+			}
+		}
+	}
+	if !token.Valid {
+		return token, xerror.New(TokenInvalid.Code(), "令牌无效")
+	}
+
+	return token, nil
 }
